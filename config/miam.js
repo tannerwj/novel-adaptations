5b932c19ab22c1cf8deb8b778b792e22a9af407d
Tanner Satchwell <tannersatch@gmail.com>
2016-12-06 21:16:46 -0700

    make it a movie

---
+++ config/miam.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const MiamSchema = new Schema({
+  goodreads_id: { type: Number, unique: true },
+  title: String,
+  image_url: String,
+  description: String,
+  publication_date: String,
+  average_rating: String,
+  authors: String,
+  essential_movie_parts: [{
+    description: String,
+    total_votes: Number,
+    votes: [{
+      user_id : { type: Number, unique: true },
+      vote: Number
+    }]
+  }],
+  nonessential_movie_parts: [{
+    description: String,
+    total_votes: Number,
+    votes: [{
+      user_id : { type: Number, unique: true },
+      vote: Number
+    }]
+  }],
+  comments: [{
+    comment: String,
+    user_id: Number,
+  }]
+})
+
+module.exports = mongoose.model('Miam', MiamSchema)
